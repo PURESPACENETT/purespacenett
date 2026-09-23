88b0dd7ca04d5197763e2aaf37b35aa5ba27edc5
PURESPACENETT <contact@purespacenett.com>
2026-09-23 08:20:27 +0200

    Enregistre la nouvelle route Entreprises B2B

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -20,7 +20,7 @@ import { Route as FaqRouteImport } from './routes/faq'
 import { Route as MentionsLegalesRouteImport } from './routes/mentions-legales'
 import { Route as PolitiqueConfidentialiteRouteImport } from './routes/politique-confidentialite'
 import { Route as SitemapDotxmlRouteImport } from './routes/sitemap[.]xml'
-import { Route as SousTraitanceRouteImport } from './routes/sous-traitance'
+import { Route as EntreprisesRouteImport } from './routes/entreprises'
 import { Route as TarifsRouteImport } from './routes/tarifs'
 import { Route as AuthenticatedAdminRouteImport } from './routes/_authenticated/admin'
 import { Route as ServicesIndexRouteImport } from './routes/services.index'
@@ -86,9 +86,9 @@ const SitemapDotxmlRoute = SitemapDotxmlRouteImport.update({
   path: '/sitemap.xml',
   getParentRoute: () => rootRouteImport,
 } as any)
-const SousTraitanceRoute = SousTraitanceRouteImport.update({
-  id: '/sous-traitance',
-  path: '/sous-traitance',
+const EntreprisesRoute = EntreprisesRouteImport.update({
+  id: '/entreprises',
+  path: '/entreprises',
   getParentRoute: () => rootRouteImport,
 } as any)
 const TarifsRoute = TarifsRouteImport.update({
@@ -150,9 +150,9 @@ export interface FileRoutesByFullPath {
   '/politique-confidentialite': typeof PolitiqueConfidentialiteRoute
   '/sitemap.xml': typeof SitemapDotxmlRoute
   '/tarifs': typeof TarifsRoute
-  '/sous-traitance': typeof SousTraitanceRoute
-  '/sous-traitance': typeof SousTraitanceRoute
-  '/sous-traitance': typeof SousTraitanceRoute
+  '/entreprises': typeof EntreprisesRoute
+  '/entreprises': typeof EntreprisesRoute
+  '/entreprises': typeof EntreprisesRoute
   '/admin': typeof AuthenticatedAdminRoute
   '/services/$slug': typeof ServicesSlugRoute
   '/zones/$slug': typeof ZonesSlugRoute
@@ -286,7 +286,7 @@ export interface RootRouteChildren {
   PolitiqueConfidentialiteRoute: typeof PolitiqueConfidentialiteRoute
   SitemapDotxmlRoute: typeof SitemapDotxmlRoute
   TarifsRoute: typeof TarifsRoute
-  SousTraitanceRoute: typeof SousTraitanceRoute
+  EntreprisesRoute: typeof EntreprisesRoute
   ServicesSlugRoute: typeof ServicesSlugRoute
   ZonesSlugRoute: typeof ZonesSlugRoute
   ServicesIndexRoute: typeof ServicesIndexRoute
@@ -375,11 +375,11 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof SitemapDotxmlRouteImport
       parentRoute: typeof rootRouteImport
     }
-    '/sous-traitance': {
-      id: '/sous-traitance'
-      path: '/sous-traitance'
-      fullPath: '/sous-traitance'
-      preLoaderRoute: typeof SousTraitanceRouteImport
+    '/entreprises': {
+      id: '/entreprises'
+      path: '/entreprises'
+      fullPath: '/entreprises'
+      preLoaderRoute: typeof EntreprisesRouteImport
       parentRoute: typeof rootRouteImport
     }
     '/tarifs': {
@@ -472,7 +472,7 @@ const rootRouteChildren: RootRouteChildren = {
   PolitiqueConfidentialiteRoute: PolitiqueConfidentialiteRoute,
   SitemapDotxmlRoute: SitemapDotxmlRoute,
   TarifsRoute: TarifsRoute,
-  SousTraitanceRoute: SousTraitanceRoute,
+  EntreprisesRoute: EntreprisesRoute,
   ServicesSlugRoute: ServicesSlugRoute,
   ZonesSlugRoute: ZonesSlugRoute,
   ServicesIndexRoute: ServicesIndexRoute,
