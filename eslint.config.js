81b37260d9d94d9138f97f5b67d747b33186ee29
PURESPACENETT <contact@purespacenett.com>
2026-09-23 07:41:40 +0200

    chore(ci): keep formatting issues as lint warnings

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import js from "@eslint/js";
+import js from "eslint/js";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -34,6 +34,7 @@ export default tseslint.config(
       ],
       "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
       "@typescript-eslint/no-unused-vars": "off",
+      "prettier/prettier": "warn",
     },
   },
   eslintPluginPrettier,
