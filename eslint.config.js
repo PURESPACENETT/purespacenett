c67ac293a5adfc4e6864b680f74eefea9a25d666
PURESPACENETT <contact@purespacenett.com>
2026-09-23 07:42:40 +0200

    fix(ci): use explicit eslint js config package

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,4 @@
-import js from "eslint/js";
+import js from "@eslint/js";
 import prettier from "eslint-plugin-prettier";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
